89da5822309d2b6d7d1e925830b7d6f8bea2cf45
Janine Luk <janineluk95@gmail.com>
2019-01-06 00:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your code in this file!
-const currentUser = 'me';
+const currentUser = 'Grace Hopper';
 
 const welcomeMessage = 'Welcome to Flatbook, currentUser!';
 
 const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser!';
 
-const shortGreeting = 'Welcome, ${currentUser}!'
+const shortGreeting = 'Welcome, ${currentUser}.slice(5)!'
