a95ed5e92a1d42f1c17fe4baa3e106f6dfee5f8f
Janine Luk <janineluk95@gmail.com>
2019-01-06 00:31:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const currentUser = 'me';
 
 const welcomeMessage = 'Welcome to Flatbook, currentUser!';
 
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser! '
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, currentUser!';
+
+const shortGreeting = 'Welcome, currentUser'
